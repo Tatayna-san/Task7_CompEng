628fc01c4ca73f2ff7d6b46db0fd932807434cfe
Tatayna-san <tany.pligynova@gmail.com>
2023-12-09 08:45:02 +0300

    Добавьте файлы проекта.

---
+++ Task7_CompEng/Fib.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10; // Количество чисел Фибоначчи, которые мы хотим вычислить
+
+            int a = 0;
+            int b = 1;
+
+            Console.WriteLine("Первые " + n + " чисел Фибоначчи:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a + " ");
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+        }
+    }
+}
