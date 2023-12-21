417bf4e13b9965ad33f486a913dc9307476ae236
Tatyana <84284073+Tatayna-san@users.noreply.github.com>
2023-12-21 21:48:43 +0300

    Add files via upload

--- test_factorial.py
+++ test_factorial.py
@@ -1,21 +1,12 @@
 import pytest
+import main
 
-def factorial(n):
-    if n == 1:
-        return 1
-    elif n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-if __name__ == '__main__':
-    print(factorial(10))
 
 def test_factorial1():
-    assert factorial(1) == 1
+    assert main.factorial(1) == 1
 
 def test_factorial5():
-    assert factorial(5) == 120
+    assert main.factorial(5) == 120
 
 def test_factorial10():
-    assert factorial(10) == 3628800
+    assert main.factorial(10) == 3628800
